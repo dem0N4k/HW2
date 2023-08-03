7cc1b602a78f3ea65701ff5e937440402e921463
dem0N4k <demON4k1993@gmail.com>
2023-08-04 02:38:31 +0300

    add hw2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*****************************************");
+        System.out.println("*****\t\t\t demON4k \t\t\t*****");
+        System.out.println("*****\t\t   город Минск   \t\t*****");
+        System.out.println("*****\t\tпроспект Пушкина\t\t*****");
+        System.out.println("*****************************************");
+    }
+}
